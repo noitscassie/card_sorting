a9e16a06c8b97679269940cbb7d998d6dccd579a
peterwdj <Peter Johnstone>
2021-03-14 19:05:54 +0000

    Simple extension to account for the instruction manual and the jokers

--- app.rb
+++ app.rb
@@ -27,19 +27,31 @@ CARDS = NUMBERS.flat_map do |number|
   end
 end
 
-DECK = CARDS.shuffle
 
-deck = DECK.dup
+def generate_deck(include_jokers:, include_instructions:)
+  deck = CARDS
+  if include_jokers
+    deck << 'Black Joker'
+    deck << 'Red Joker'
+  end
+  deck << 'Instruction Manual' if include_instructions
+  deck.shuffle
+end
 
 def request(card)
   [
     "The #{card}, please and thank you",
     "Can I have the #{card}, please",
     "#{card}, please",
+    "The #{card}, please",
     "Next is the #{card}",
     card,
+    "The #{card}",
+    "And now the #{card}",
   ].sample
 end
+
+deck = generate_deck(include_jokers: true, include_instructions: true)
 
 until deck.length == 0
   command = gets.chomp
